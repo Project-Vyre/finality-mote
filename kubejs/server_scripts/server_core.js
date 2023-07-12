428c55a1716109781a3556ec02b9029cb820f93c
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-07-12 04:11:10 -0400

    Finality 0.1.3 pressure plate recipe adjustments

--- kubejs/server_scripts/server_core.js
+++ kubejs/server_scripts/server_core.js
@@ -129,10 +129,16 @@ ServerEvents.recipes(event => {
 		S: 'extendedcrafting:black_iron_ingot'
 	}).id('finality:crafting/final_hoe')
 	STONEPLATES.forEach(stone => {
-		event.recipes.create.cutting([`${stone}_pressure_plate`, `${stone}_slab`], `${stone}`).id(`minecraft:${stone}_pressure_plate`)
+		event.stonecutting(`${stone}_pressure_plate`, `${stone}_slab`).id(`minecraft:${stone}_pressure_plate`)
 	})
 	WOODPLATES.forEach(wood => {
-		event.recipes.create.cutting([`${wood}_pressure_plate`, `${wood}_slab`], `${wood}_planks`).id(`minecraft:${wood}_pressure_plate`)
+		event.stonecutting(`${wood}_pressure_plate`, `${wood}_slab`).id(`minecraft:${wood}_pressure_plate`)
+	})
+	STONEPLATES.forEach(stone => {
+		event.recipes.create.cutting([`${stone}_pressure_plate`, `${stone}_slab`], `${stone}`).id(`finality:${stone}_pressure_plate`)
+	})
+	WOODPLATES.forEach(wood => {
+		event.recipes.create.cutting([`${wood}_pressure_plate`, `${wood}_slab`], `${wood}_planks`).id(`finality:${wood}_pressure_plate`)
 	})
 	event.shaped('light_weighted_pressure_plate', [
 		'G',
